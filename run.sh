ee8f464d94576a4794207a79ecb8ccaf3edf5ef4
Brooklyn <yuanotes@gmail.com>
2015-11-11 12:41:35 +0800

    Copy files to predefined path.

--- run.sh
+++ run.sh
@@ -2,10 +2,24 @@
 chown www-data:www-data /app -R
 
 if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
-	unset ALLOW_OVERRIDE
+    unset ALLOW_OVERRIDE
 else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
     a2enmod rewrite
+fi
+
+if [ -d "$KOD_SYSTEM_PATH" ] ; then
+    if [ "$(ls -A $KOD_SYSTEM_PATH)" ]; then
+        cp /app/data/system/* "$KOD_SYSTEM_PATH/"
+        echo "Copy system files."
+    fi
+fi
+
+if [ -d "$KOD_USER_PATH" ] ; then
+    if [ "$(ls -A $KOD_USER_PATH)" ]; then
+        cp /app/data/User/* "$KOD_USER_PATH/"
+        echo "Copy user files."
+    fi
 fi
 
 source /etc/apache2/envvars
